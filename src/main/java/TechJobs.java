d40a040b3fd9c3adbc57008b2725ac6c52c365e9
trappmichael <142745435+trappmichael@users.noreply.github.com>
2023-12-11 19:29:56 -0600

    Updated Unit 2 Graded Assignment 1

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -121,7 +121,7 @@ public class TechJobs {
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
         if (someJobs.size() == 0) {
-            System.out.println("No Results");
+            System.out.print("No Results");
         } else {
             for (HashMap<String,String> job : someJobs) {
                 System.out.println("\n*****");
@@ -133,3 +133,4 @@ public class TechJobs {
         }
     }
 }
+
